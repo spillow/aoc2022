32df645d89246278f7153d4b7655fdf8d6b5c18c
spillow <scott.pillow@gmail.com>
2022-12-03 22:58:44 -0800

    day 1.

--- days/src/day01.rs
+++ days/src/day01.rs
@@ -1,7 +1,33 @@
+use std::io::{BufRead};
+use std::{fs::File, io::BufReader};
+
+fn get_calorie_list(path: &str) -> Vec<i32> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut calories = Vec::new();
+    let mut curr_sum = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.trim() == "" {
+            calories.push(curr_sum);
+            curr_sum = 0;
+            continue
+        }
+        let num: i32 = line.parse().unwrap();
+        curr_sum += num;
+    }
+    calories
+}
+
 pub fn part_a() {
-    println!("Hello, A!");
+    let cals = get_calorie_list("inputs/day01/input.txt");
+    println!("Part A: Max calories is {}", cals.iter().max().unwrap());
 }
 
 pub fn part_b() {
-    println!("Hello, B!");
+    let mut cals = get_calorie_list("inputs/day01/input.txt");
+    cals.sort_by(|a, b| b.cmp(a));
+    let top_three = cals[0] + cals[1] + cals[2];
+    println!("Part B: Sum of top three calories is {}", top_three);
 }
--- days/src/main.rs
+++ days/src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 
 fn main() {
+    println!("**Day 1**");
     day01::part_a();
     day01::part_b();
 }
